b36e2893460276cc126ea33faae10fdf2b7d7d70
Thomas Kaemmerling <thomas.kaemmerling@meta-level.de>
2022-10-26 18:13:33 +0200

    appname changed

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular11-sample-app'),
+      dir: require('path').join(__dirname, './coverage/tracing_app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
