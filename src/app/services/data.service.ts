df4f837cb637b8def179c3e14f82140ea8407aa5
Thomas Kaemmerling <thomas.kaemmerling@meta-level.de>
2022-11-10 16:55:02 +0100

    URL TOKEN

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,7 +9,7 @@ export class DataService {
 constructor(private httpClient:HttpClient) { }
 
 public receiveMachineQuotes(){
- return this.httpClient.get('https://tracedatareceiver20221103164225.azurewebsites.net/api/MachineQuote').toPromise();
+ return this.httpClient.get('https://tracedatareceiver20221103164225.azurewebsites.net/api/MachineQuote?code=DiJH6CD8xmbbfhCRZFmUnyWA2WrTlTuTT0SMCWrYiTp2AzFuyOc92A==').toPromise();
 }
 
 }
