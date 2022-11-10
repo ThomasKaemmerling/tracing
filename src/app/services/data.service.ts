4ad534f9b0619b49b2437ef0174e3b159fd9bb41
Thomas Kaemmerling <thomas.kaemmerling@meta-level.de>
2022-11-10 16:35:02 +0100

    URL Dataservice angepasst

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,7 +9,7 @@ export class DataService {
 constructor(private httpClient:HttpClient) { }
 
 public receiveMachineQuotes(){
- return this.httpClient.get('https://tracedatareceiver20221103164225.azurewebsites.net/api/Function1?code=meK_e66C5mb5X8yqJP3cxIBsvgVCHO7o9Y_KvQ9dN11FAzFunwmtTQ==').toPromise();
+ return this.httpClient.get('https://tracedatareceiver20221103164225.azurewebsites.net/api/MachineQuote').toPromise();
 }
 
 }
