fc84413573410936a8fd885da42fc13fa8900c05
Christopher Biehl <christopher.biehl@meta-level.de>
2022-12-01 16:48:43 +0100

    Coloring report graph

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,29 +1,38 @@
 import { DataService } from './../services/data.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Query } from '@angular/core';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.scss']
+  styleUrls: ['./report.component.scss'],
 })
 export class ReportComponent implements OnInit {
-
   data: any;
 
-  constructor(private dataService:DataService) {
-  }
+  constructor(private dataService: DataService) {}
 
   async ngOnInit() {
     const quotes = await this.dataService.receiveMachineQuotes();
     this.data = {
-      labels:['Quote OK Parts'],
-      datasets:[]
+      labels: ['Quote OK Parts'],
+      datasets: [],
+    };
+    for (const prop in quotes) {
+      const quote = Number((quotes as any)[prop]);
+      this.data.datasets.push({
+        label: prop,
+        backgroundColor: this.getBarColor(quote),
+        data: [quote],
+      });
     }
-    for(const prop in quotes){
-      this.data.datasets.push({label:prop,data:[Number((quotes as any)[prop])]})
-    }
-
-
   }
 
+  private getBarColor(quote: number) {
+    if (quote < 90)
+      return 'red';
+    if (quote < 95)
+      return '#FFCA28'; // yellow
+
+    return '#66BB6A'; // green
+  }
 }
